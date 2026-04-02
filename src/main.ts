cb3dd7b6bbd6067912a73877ae1ae044310b3e0c
Sachin <sachinnegi899@gmail.com>
2026-04-02 23:58:39 +0530

    initial load changes

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,9 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
+  .then(() => {
+    window.requestAnimationFrame(() => {
+      document.body.classList.add('loaded');
+    });
+  })
   .catch((err) => console.error(err));
